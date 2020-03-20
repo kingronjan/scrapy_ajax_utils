67a9005fedef987e7732051f8bdfcb035629e1b4
financial <financialfly@outlook.com>
2020-03-20 14:39:14 +0800

    updated.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scrapy_ajax_utils',
-    version=0.123,
+    version=0.124,
     description=(
         'ajax utils for scrapy.'
     ),
