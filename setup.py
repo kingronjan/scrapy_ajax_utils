5d687c1b5b941f9067cf120c5bfe1502a638722e
kingron <financialfly@outlook.com>
2020-09-01 09:23:21 +0800

    updated to 1268.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scrapy_ajax_utils',
-    version=0.1267,
+    version=0.1268,
     description=(
         'ajax utils for scrapy.'
     ),
